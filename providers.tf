5265f5552b6f91fece5819ea7188a524eb1eaae4
Cameron L Cleveland <cameron.cleveland@ymail.com>
2026-01-30 03:41:12 -0600

    updated providers block

--- providers.tf
+++ providers.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.85"
     }
+    # ADDED: Requirement for AzureAD provider
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
@@ -15,6 +20,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_cli = false
-  skip_provider_registration = true  # ADD THIS LINE
+  use_cli                    = false
+  skip_provider_registration = true
+}
+
+# ADDED: Explicit configuration for AzureAD
+provider "azuread" {
+  # This will automatically use the same ARM_ credentials as azurerm
 }
